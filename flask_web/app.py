c3e9303fdcc8cca92c13bc265410feb272627f01
lukasscholz <lukas.scholz@hwr-berlin.de>
2022-02-18 18:09:11 +0100

    added password checking-mechanism

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, request
+
 from controller.database import get_conn
+from controller.encryption import verify_password
+
 from model.registration import create_user
 
 app = Flask(__name__)
@@ -11,13 +14,25 @@ def welcome():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def my_form_post():
+def route_create_user():
     name = request.form['name']
     lastname = request.form['lastname']
     email = request.form['email']
     password = request.form['password']
     create_user(name, lastname, email, password)
-    return {'Message': 'OK'}
+    return render_template('index.html')
+
+# login
+@app.route('/login')
+def check():
+    return render_template('pwd.html')
+
+@app.route('/check', methods=['POST'])
+def route_verify_password():
+    email = request.form['email']
+    password = request.form['password']
+    val = verify_password(email, password)
+    return {'Msg': val}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
