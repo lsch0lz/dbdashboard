74e981a10fb9be02063a04270448cdc41a94b201
lukasscholz <lukas.scholz@hwr-berlin.de>
2022-02-18 18:09:11 +0100

    added password checking-mechanism

--- flask_web/controller/database.py
+++ flask_web/controller/database.py
@@ -11,14 +11,3 @@ def get_conn() -> mysql.connector.connection_cext.CMySQLConnection:
         return conn
     except Error as e:
         print("Error while connecting to MySQL", e)
-
-# registers a new user to the database
-# TODO: hash the password
-def create_user(name: str, lastname: str, email: str, password: str):
-    conn = get_conn()
-    cursor = conn.cursor()
-    query = "INSERT INTO user(name, lastname, email, password) VALUES (%s, %s, %s, %s)"
-    val = (name, lastname, email, password)
-    cursor.execute(query, val)
-    conn.commit()
-    return None
