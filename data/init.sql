a675758bece9b04dd4a78c5cd6475fbc7056cc74
lukasscholz <lukas.scholz@hwr-berlin.de>
2022-03-04 11:56:04 +0100

    changed to postgres

--- data/init.sql
+++ data/init.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS dbdashboard;
-USE dbdashboard;
-CREATE TABLE IF NOT EXISTS user (id int NOT NULL auto_increment, name varchar(255), lastname varchar(255), email varchar(255), password varchar(255), PRIMARY KEY (id));
+CREATE DATABASE dbdashboard;
+\c dbdashboard;
+CREATE TABLE users ( id serial PRIMARY KEY, name VARCHAR ( 50 ) UNIQUE NOT NULL, lastname VARCHAR ( 50 ) NOT NULL, email VARCHAR ( 255 ) UNIQUE NOT NULL, password VARCHAR ( 255 ) NOT NULL);
