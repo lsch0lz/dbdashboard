e4beb40035cf1295c80ec3f93e0eebe0dfb3e4be
lukasscholz <lukas.scholz@hwr-berlin.de>
2022-03-04 16:21:31 +0100

    changed login for hosting

--- flask_web/controller/database.py
+++ flask_web/controller/database.py
@@ -1,13 +1,15 @@
-import mysql.connector
-from mysql.connector import Error
+import psycopg2
+from psycopg2 import Error
+import os
 
 # returns connection to database 'dbdashboard'
-def get_conn() -> mysql.connector.connection.MySQLConnection:
-    try:
-        conn = mysql.connector.connect(host='db',
-                                             database='dbdashboard',
-                                             user='root',
-                                             password='root')
-        return conn
-    except Error as e:
-        print("Error while connecting to MySQL", e)
+def get_conn():
+    pwd = os.getenv('postgres_password')
+    print(pwd)
+    conn = psycopg2.connect(host='app-32a76287-24f4-4343-b19e-c1bcba546d72-do-user-11028986-0.b.db.ondigitalocean.com',
+                            database='dbdashboard',
+                            user='dbdashboard',
+                            password=pwd,
+                            port=25060)
+    return conn
+
