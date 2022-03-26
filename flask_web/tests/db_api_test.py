4614ae82fcc4fd92e14ae942d71b46983b2c6c42
lukasscholz <lukas.scholz@hwr-berlin.de>
2022-03-26 09:51:21 +0100

    testcase for db_api

--- flask_web/tests/db_api_test.py
+++ flask_web/tests/db_api_test.py
@@ -1,6 +1,14 @@
 import pytest
 import requests
 
-def test_db_api():
+def test_db_locations():
     response = requests.get("https://api.deutschebahn.com/freeplan/v1/location/Berlin")
+    assert response.status_code == 200
+
+def test_db_arrivalBoard():
+    response = requests.get("https://api.deutschebahn.com/freeplan/v1/arrivalBoard/8011160?date=2022-01-01")
+    assert response.status_code == 200
+
+def test_db_departureBoard():
+    response = requests.get("https://api.deutschebahn.com/freeplan/v1/departureBoard/8011160?date=2022-01-01")
     assert response.status_code == 200
