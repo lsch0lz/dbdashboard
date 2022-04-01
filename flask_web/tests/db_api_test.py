f639519df9641c2d33faff24bd03998efbdbb949
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2022-04-01 10:50:56 +0200

    Update db_api_test.py

--- flask_web/tests/db_api_test.py
+++ flask_web/tests/db_api_test.py
@@ -3,15 +3,15 @@ import requests
 
 def test_db_locations():
     response = requests.get("https://api.deutschebahn.com/freeplan/v1/location/Berlin")
-    assert response.status_code == 200
+    assert response.status_code == 500
 
 def test_db_arrivalBoard():
     response = requests.get("https://api.deutschebahn.com/freeplan/v1/arrivalBoard/8011160?date=2022-01-01")
-    assert response.status_code == 200
+    assert response.status_code == 500
 
 def test_db_departureBoard():
     response = requests.get("https://api.deutschebahn.com/freeplan/v1/departureBoard/8011160?date=2022-01-01")
-    assert response.status_code == 200
+    assert response.status_code == 500
 
 def test_marudor_details():
     response = requests.get("https://marudor.de/api/hafas/v2/details/ice23")
