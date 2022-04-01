8e329432d069e991b25c0555239fe43136f83a91
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2022-04-01 10:35:49 +0200

    Update db_api_test.py

--- flask_web/tests/db_api_test.py
+++ flask_web/tests/db_api_test.py
@@ -12,3 +12,11 @@ def test_db_arrivalBoard():
 def test_db_departureBoard():
     response = requests.get("https://api.deutschebahn.com/freeplan/v1/departureBoard/8011160?date=2022-01-01")
     assert response.status_code == 200
+
+def test_marudor_details():
+    response = requests.get("https://marudor.de/api/hafas/v2/details/ice23")
+    assert response.status_code == 200
+
+def test_marudor_reihung():
+    response = requests.get("https://marudor.de/api/reihung/v4/runsPerDate/2022-03-24T11:50:00.000Z")
+    assert response.status_code == 200
