c44eaae30eac46cd8cca891232d29b2e058b5b41
therealgherkhin <lscholz14@googlemail.com>
2022-04-05 15:32:12 +0200

    added new design

---
+++ flask_web/templates/script.js
@@ -0,0 +1,11 @@
+<script>
+  
+$('#darkmode').click(function(){
+
+  
+ $('body').css( "background-color", "black")
+ $('body').css( "color", "white")
+ $('.darkmode-text').text("Darkmode deaktivieren ");
+
+}); 
+</script>
